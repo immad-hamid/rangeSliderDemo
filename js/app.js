111d8e2d82fd31e5645005b91f487a1f92f7d15c
Immad Hamid <immad.hamid@gmail.com>
2018-08-20 16:50:21 +0500

    - Fixed the range values - Added calculation method - Did all the required css for the range slider design - removed the cdns and created local files against it

--- js/app.js
+++ js/app.js
@@ -1,18 +1,45 @@
-var $range = $(".js-range-slider"),
+document.addEventListener('DOMContentLoaded', removeSmallPolls, false);
+
+function removeSmallPolls() {
+  document.querySelectorAll('.irs-grid-pol.small').forEach((el) => {
+    el.remove();
+  });
+}
+
+let $range = $(".js-range-slider"),
     $result = $(".js-result"),
     $getvalues = $(".js-get-values"),
-    
     from = 1;
 
-var saveResult = function (data) {
+let saveResult = function (data) {
     from = data.from;
     to = data.to;
+    if (from === 1) {
+      removeSmallPolls();
+      document.querySelectorAll('.irs-grid-pol').forEach(el => el.style.borderColor = '#cfd8de');
+    }
+    else if (from === 2) {
+      document.querySelectorAll('.irs-grid-pol').forEach((el, i) => {
+        (i < 2) ? el.style.borderColor = '#10ebb1' : el.style.borderColor = '#cfd8de'
+      });
+    }
+    else if (from === 3) {
+      document.querySelectorAll('.irs-grid-pol').forEach((el, i) => {
+        (i < 3) ? el.style.borderColor = '#10ebb1' : el.style.borderColor = '#cfd8de'
+      });
+    }
+    else document.querySelectorAll('.irs-grid-pol').forEach(el => el.style.borderColor = '#10ebb1')
+    
     writeResult();
 };
 
 var writeResult = function () {
-    var result = "from: " + from + ", to: " + to;
-    $result.html(result);
+  let result;
+  if (from === 1) result = `$${(5 * 50)}`;
+  if (from === 2) result = `$${(20 * 75)}`;
+  if (from === 3) result = `$${(45 * 100)}`;
+  if (from === 4) result = `$${(80 * 100)}`;
+  $result.html(result);
 };
 
 $range.ionRangeSlider({
@@ -36,24 +63,3 @@ $range.ionRangeSlider({
 });
 
 $getvalues.on("click", writeResult);
-
-
-
-// $('#range_min_prefix').ionRangeSlider({
-//     // min: 10000,
-//     // max: 75000,
-//     // from: 25000,
-//     // step: 5000,
-//     grid: true,
-//     grid_snap: true,
-//     force_edges: true,
-//     prefix: '$',
-//     min_prefix: '< ',
-//     max_postfix: '+',
-//     decorate_both: false,
-//     prettify_enabled: true,
-//     prettify_separator: ',',
-//     hide_min_max: true,
-//     values: ['Womans', 'Mostly Womans', 'Any', 'Mostly Mens', 'Mens']
-//   });
-  
